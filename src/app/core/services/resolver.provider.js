fc7f762b016e06b2b65dd3e1daf5987e881c50cf
dasein <daseinpwt@gmail.com>
2018-02-08 09:54:31 +0100

    remove 'no-trailing-space' from eslint rules

--- src/app/core/services/resolver.provider.js
+++ src/app/core/services/resolver.provider.js
@@ -13,6 +13,7 @@ export default function(app) {
     'ngInject';
 
     const deferred = $q.defer();
+
     require.ensure([], (require) => {
       const asyncModule = require('../../pages/async-page-example/async.module');
       $ocLazyLoad.load({
