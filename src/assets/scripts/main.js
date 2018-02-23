482a22f8266cac0a82604b8a0b7d7f8b0f249502
Ruben Helsloot <rht670@student.vu.nl>
2018-02-23 17:32:18 +0100

    Remove angularJS and relevant dependencies, restructure codebase
    This commit introduces the notion of class based visualizations, where
    one central place stores the instances and where interactivity can be
    managed.

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,9 @@
+// Import dependent classes
+import BarChart from '_scripts/barchart.js';
+
+// Import data
+import barchart_data from '_data/barchart-data.tsv';
+
+// Initialize classes and assign their instances to variables.
+// This way, we can make them interact.
+const bar = new BarChart(barchart_data);
