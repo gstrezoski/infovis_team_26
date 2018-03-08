d2237f7b7222869ea64caf2540e6aba78e59c400
Ruben Helsloot <rht670@student.vu.nl>
2018-03-08 01:33:42 +0100

    Implement colored hulls correctly, start on controls

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,13 +1,15 @@
 // Import dependent classes
-import BarChart from '_scripts/barchart.js';
+// import BarChart from '_scripts/barchart.js';
 import TreeChart from '_scripts/tree.js';
+import Controls from '_scripts/controls.js';
 
 // Import data
-import barchart_data from '_data/barchart-data.tsv';
+// import barchart_data from '_data/barchart-data.tsv';
 import core_2015_01 from '_data/core_2015-01.csv';
 
 // Initialize classes and assign their instances to variables.
 // This way, we can make them interact.
-const bar = new BarChart(barchart_data);
+const ctrl = new Controls();
+// const bar = new BarChart(barchart_data);
 
 const tree = new TreeChart(core_2015_01);
--- src/assets/scripts/tree.js
+++ src/assets/scripts/tree.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import Hull from '_scripts/hull.js';
 
 export default class TreeChart {
 
@@ -8,13 +9,15 @@ export default class TreeChart {
     this.svg = d3.select('svg.tree');
 
     this.treelayer = this.svg.append('g').attr('class', 'treelayer');
+    this.hulllayer = this.svg.append('g').attr('class', 'hulllayer');
+    this.uilayer = this.svg.append('g').attr('class', 'uilayer');
     this.stratify = d3.stratify().parentId((d) => d.solid_line);
 
-    this.tree = d3.tree()
-      .size([2 * Math.PI, 100])
-      .separation((a, b) => (a.parent === b.parent ? 1 : 2) / a.depth);
+    this.groupBy = 'location';
+    this.groupByLevel = 1;
 
-    this.render();
+    this.tree = d3.tree().separation((a, b) => (a.parent === b.parent ? 1 : 2) / a.depth);
+
     this.resize();
 
     window.addEventListener('resize', () => { this.resize(); });
@@ -33,47 +36,53 @@ export default class TreeChart {
       this.height = box['height'];
     }
 
-    console.log(oldValues, this.width, this.height);
+    this.tree.size([2 * Math.PI, Math.min(this.width, this.height) / 2.2]);
 
     if (oldValues[0] !== this.width && oldValues[1] !== this.height) {
-      if (this._rendered) {
-        this.relocate()
-      } else {
-        this.render();
-      }
+      this.render();
+      // if (this._rendered) {
+      //   this.relocate()
+      // } else {
+      //   this.render();
+      // }
     }
   }
 
   render() {
     d3.text(this.dataSrc, (error, raw) => {
       if (error) { throw error; }
-      const data = d3.dsvFormat(';').parse(raw)
+      this.data = d3.dsvFormat(';').parse(raw)
         .map((d) => ({
-          id: +d['job_id'],
-          solid_line: +d['solid_line'] || '',
-          name: `${d['first_name']} ${d['last_name']}`,
-          gender: d['gender'],
+          id: +d.job_id,
+          solid_line: +d.solid_line || '',
+          name: `${d.first_name} ${d.last_name}`,
+          gender: d.gender,
+
+          location_level0: d.location_level0,
+          location_level1: d.location_level1,
+          location_level2: d.location_level2,
+          location_level3: d.location_level3,
         }));
       
-      const root = this.tree(this.stratify(data).sort((a, b) => a.height - b.height || a.children - b.children));
-      this.links = this.treelayer.selectAll('.link')
-        .data(root.links())
-        .enter()
-        .append('path')
-        .attr('class', 'link')
-        .attr('d', d3.linkRadial().angle(root.x)
-          .radius(root.y));
+      this.root = this.tree(this.stratify(this.data).sort((a, b) => this.sort(a, b)));
 
-      this.nodes = this.treelayer.selectAll('.node')
-        .data(root.descendants())
-        .enter()
-        .append('g')
+      this.links = this.treelayer.selectAll('.link').data(this.root.links());
+      this.links.exit().remove();
+      const newLinks = this.links.enter().append('path')
+        .classed('link', 'true');
+      this.links = this.links.merge(newLinks);
+
+      this.nodes = this.treelayer.selectAll('.node').data(this.root.descendants());
+      this.nodes.exit().remove();
+      const newNodes = this.nodes.enter().append('g');
+      this.nodes = this.nodes.merge(newNodes)
         .attr('class', (d) => d.children ? 'node node-interal' : 'node node-leaf');
       
       this.nodes.append('circle')
-        .attr('r', (d) => 2 + root.height - d.depth)
+        .attr('r', (d) => 2 + this.root.height - d.depth)
         .attr('class', (d) => d.data.gender);
 
+      this.group();
       this._rendered = true;
       this.relocate();
     });
@@ -82,19 +91,79 @@ export default class TreeChart {
   relocate() {
     this.links
       .transition()
-      .duration(500)
+      .duration(1500)
       .attr('d', d3.linkRadial().angle((d) => d.x)
-        .radius((d) => d.y * this.height / 100));
+        .radius((d) => d.y));
     this.nodes
       .transition()
-      .duration(500)
+      .duration(1500)
       .attr('transform', (d) => `translate(${this.locate(d)})`);
+    this.hulls
+      .transition()
+      .duration(1500)
+      .attr('d', (d) => d.d);
   }
 
   locate(d) {
-    const y = +d.y * this.height / 100,
-      x = +d.x - Math.PI / 2;
+    const x = d.x - Math.PI / 2;
+    return [d.y * Math.cos(x), d.y * Math.sin(x)];
+  }
+
+  sort(a, b) {
+    const av = a.data[`${this.groupBy}_level${this.groupByLevel}`],
+      bv = b.data[`${this.groupBy}_level${this.groupByLevel}`];
+    return av.localeCompare(bv);
+  }
+
+  getFirstDifferentManager(d) {
+    let p = d.parent;
+    if (p && this.sort(p, d) === 0) {
+      return this.getFirstDifferentManager(p);
+    }
+    return p ? p.data.name : d.data.name;
+  }
+
+  group() {
+    const groups = d3.nest()
+      .key((d) => d.data[`${this.groupBy}_level${this.groupByLevel}`])
+      .key((d) => this.getFirstDifferentManager(d))
+      .entries(this.root.descendants());
     
-    return [y * Math.cos(x), y * Math.sin(x)];
+    this.hullKeys = groups.map((g) => g.key)
+    this.hullObjs = [];
+    groups.forEach((g) => {
+      Object.values(g.values).forEach((v) => {
+        this.hullObjs.push(new Hull(g.key, v.values.map((v) => this.locate(v))));
+      });
+    });
+
+    this.hulls = this.hulllayer.selectAll('.hull').data(this.hullObjs);
+    this.hulls.exit().remove();
+    const newHulls = this.hulls.enter().append('path')
+      .classed('hull', true);
+    this.hulls = this.hulls.merge(newHulls).attr('d', (d) => d.d)
+      .attr('fill', (d) => d3.interpolateWarm(this.hullKeys.indexOf(d.id) / this.hullKeys.length));
+    
+    this.legend();
+  }
+
+  legend() {
+    this.legend = this.uilayer.selectAll('.legend').data(this.hullKeys);
+    this.legend.exit().remove();
+    const newLegend = this.legend.enter().append('g')
+      .attr('class', 'legend');
+
+    this.legend = this.legend.merge(newLegend).attr('transform', (d, i) => `translate(0, ${i * 20})`);
+    this.legend.append('rect')
+      .attr('x', 20)
+      .attr('y', 20)
+      .attr('width', 18)
+      .attr('height', 18)
+      .style('fill', (d) => d3.interpolateWarm(this.hullKeys.indexOf(d) / this.hullKeys.length));
+    
+    this.legend.append('text')
+      .attr('x', 42)
+      .attr('y', 33)
+      .text((d) => d);
   }
 }
