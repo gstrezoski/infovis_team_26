fe92c7b8f04a534a95f5f63c7708700c983be49c
Ruben Helsloot <rht670@student.vu.nl>
2018-03-08 11:10:17 +0100

    Some refactoring and configurable hull grouping for main graph

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,12 +4,24 @@ import TreeChart from '_scripts/tree.js';
 import Controls from '_scripts/controls.js';
 
 // Import data
-// import barchart_data from '_data/barchart-data.tsv';
 import core_2015_01 from '_data/core_2015-01.csv';
+
+// Default variables
+const defaultVars = {
+  groupBy: 'functional_area',
+  groupByLevel: 1,
+}
 
 // Initialize classes and assign their instances to variables.
 // This way, we can make them interact.
-const ctrl = new Controls();
-// const bar = new BarChart(barchart_data);
+const ctrl = new Controls(defaultVars);
 
-const tree = new TreeChart(core_2015_01);
+const tree = new TreeChart(defaultVars).load(core_2015_01);
+
+['groupBy', 'groupByLevel'].forEach((cls) => {
+  ctrl.controls[cls].on('click', (d) => {
+    ctrl.active[cls] = d;
+    ctrl.controls[cls].classed('active', (d) => d === ctrl.active[cls]);
+    tree.setVar(cls, d);
+  });
+});
