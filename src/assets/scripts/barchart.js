cb9b40f07416adb9babe4ab52e61e96b7a847cf0
Aida <aidaploco.ap@gmail.com>
2018-03-12 15:17:53 +0100

    Linechart: Work in progress

--- src/assets/scripts/barchart.js
+++ src/assets/scripts/barchart.js
@@ -3,14 +3,35 @@ import * as d3 from 'd3';
 export default class BarChart {
 
   // Accept external data to make it reusable
-  constructor(data) {
-    this.data = data;
-    this.render();
+  constructor() {
+    this.selector = 'gender';
 
     // window.addEventListener('resize', this.render);
   }
 
+  setSelector(newValue) {
+    this.selector = newValue;
+    this.render();
+  }
+
+  load(url) {
+    this._loading = true;
+    d3.csv(this.data, (d) => {
+      d.month = new Date(d.month)
+      d.perc_women = +d.perc_women
+      return d;
+    }, (error, data) => {
+      if (error) { throw error; }
+
+      this.data = data;
+      this._loading = false;
+      this.render();
+    });
+  }
+
   render() {
+    if(this._loading !== true) return;
+
     const svg = d3.select('svg.bar-chart'),
       margin = { top: 20, right: 20, bottom: 30, left: 40 },
       box = svg.node().getBoundingClientRect();
@@ -25,7 +46,7 @@ export default class BarChart {
       height = box['height'] - margin.top - margin.bottom;
     }
 
-    const x = d3.scaleBand().rangeRound([0, width])
+    const x = d3.scaleTime().rangeRound([0, width])
         .padding(0.1),
       y = d3.scaleLinear()
         .rangeRound([height, 0]);
@@ -35,44 +56,37 @@ export default class BarChart {
     const g = svg.append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    d3.tsv(this.data, (d) => {
-      d.frequency = +d.frequency;
-      return d;
-    }, (error, data) => {
-      if (error) { throw error; }
+    x.domain(this.data.map((d) => d.month));
+    y.domain([0, d3.max(data, (d) => d.perc_women)]);
 
-      x.domain(data.map((d) => d.letter));
-      y.domain([0, d3.max(data, (d) => d.frequency)]);
+    g.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
 
-      g.append('g')
-        .attr('class', 'axis axis--x')
-        .attr('transform', `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
+    g.append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3.axisLeft(y).ticks(10, '%'))
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '0.71em')
+      .attr('text-anchor', 'end')
+      .text('Frequency');
 
-      g.append('g')
-        .attr('class', 'axis axis--y')
-        .call(d3.axisLeft(y).ticks(10, '%'))
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 6)
-        .attr('dy', '0.71em')
-        .attr('text-anchor', 'end')
-        .text('Frequency');
-
-      g.selectAll('.bar')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('x', (d) => x(d.letter))
-        .attr('y', height)
-        .attr('width', x.bandwidth())
-        .attr('height', 0)
-        // everything after this is with regard to the transition
-        .transition()
-        .duration(500)
-        .attr('y', (d) => y(d.frequency))
-        .attr('height', (d) => height - y(d.frequency));
-    });
+    g.selectAll('.bar')
+      .data(this.data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => x(d.month))
+      .attr('y', height)
+      .attr('width', x.bandwidth())
+      .attr('height', 0)
+      // everything after this is with regard to the transition
+      .transition()
+      .duration(500)
+      .attr('y', (d) => y(d.perc_women))
+      .attr('height', (d) => height - y(d.perc_women));
   }
 }
