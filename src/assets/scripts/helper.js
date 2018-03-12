981bb29d7a791a4e1737f857f613e1cff4466ffd
dasein <daseinpwt@gmail.com>
2018-03-12 19:57:53 +0100

    Encapsulate tooltip code

--- src/assets/scripts/helper.js
+++ src/assets/scripts/helper.js
@@ -1,18 +1,21 @@
 import * as d3 from 'd3';
 
-d3.select('a.float')
+d3.select('a.helper-container')
   .on('click', function() {
-    let icon = d3.select('#helper-icon');
+    let icon = d3.select('i.helper-icon');
+    let comp = d3.selectAll('.component');
+    let ht = d3.selectAll('div.helper-text');
+
     icon.classed('fa-question', !icon.classed('fa-question'));
     icon.classed('fa-times', !icon.classed('fa-times'));
 
-    let comp = d3.selectAll('.component');
-    let hp = d3.selectAll('div.helper-text');
     if (comp.style('opacity') == 0.3) {
       comp.style('opacity', 1);
-      hp.style('opacity', 0);
+      ht.style('opacity', 0);
+      ht.style('display', 'none');
     } else {
       comp.style('opacity', 0.3);
-      hp.style('opacity', 1);
+      ht.style('opacity', 1);
+      ht.style('display', 'block');
     }
   });
