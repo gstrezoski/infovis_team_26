b72443f0195c152fc03b3d17b9b77b87a903f5b8
dasein <daseinpwt@gmail.com>
2018-03-12 23:17:48 +0100

    Add age/gender metric and hover behaviour

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# infovis_team_26
+Visualizing human resource data
+
 ## Installation
 
 - Install `node`. It should automatically install `npm` as well. Run `node -v` and `npm -v` to check whether the installation has been succesfull.
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,13 +1,11 @@
 // Import dependent classes
 // import BarChart from '_scripts/barchart.js';
 import TreeChart from '_scripts/tree.js';
-import Controls from '_scripts/controls.js';
-
-// Import data
 import LineChart from '_scripts/linechart.js';
+import Controls from '_scripts/controls.js';
+import setup_helper from '_scripts/helper.js';
 
 // Import data
-import barchart_data from '_data/barchart-data.tsv';
 import line_chart_location_data from '_data/line_chart_location.csv';
 import core_2015_01 from '_data/core_2015-01.csv';
 
@@ -15,21 +13,60 @@ import core_2015_01 from '_data/core_2015-01.csv';
 const defaultVars = {
   groupBy: 'functional_area',
   groupByLevel: 1,
+  metric: 'gender',
 }
+
+// Set age range as a global variable
+window.ranges = {
+  age: [17, 70],
+};
 
 // Initialize classes and assign their instances to variables.
 // This way, we can make them interact.
 const ctrl = new Controls(defaultVars);
-
+const line = new LineChart().load(line_chart_location_data);
 const tree = new TreeChart(defaultVars).load(core_2015_01);
 
-['groupBy', 'groupByLevel'].forEach((cls) => {
-  ctrl.controls[cls].on('click', (d) => {
-    ctrl.active[cls] = d;
-    ctrl.controls[cls].classed('active', (d) => d === ctrl.active[cls]);
-    tree.setVar(cls, d);
-  });
-});
-//const bar = new BarChart(barchart_data);
-const line = new LineChart().load(line_chart_location_data);
-//const tree = new TreeChart(core_2015_01);
+const registerEvents = () => {
+  // Check if everything has been rendered correctly
+  if (!tree._rendered || !ctrl.controls) { setTimeout(registerEvents, 300); } else {
+    ['groupBy', 'groupByLevel', 'metric'].forEach((cls) => {
+      ctrl.controls[cls].on('click', (d) => {
+        ctrl.active[cls] = d;
+        ctrl.controls[cls].classed('active', (c) => c === ctrl.active[cls]);
+        tree.setVar(cls, d);
+
+        // After redrawing, reregister events
+        registerEvents();
+      });
+    });
+
+    tree.hulls.on('mouseenter', (d) => {
+      // Add logic that should happen on start of hovering over a hull here
+      tree.hulls.filter((hull) => d.id === hull.id).classed('hover', true);
+      tree.legend.filter((leg) => d.id === leg).classed('hover', true);
+    });
+
+    tree.hulls.on('mouseleave', (d) => {
+      // Add logic that should happen on end of hovering over a hull here
+      tree.hulls.filter((hull) => d.id === hull.id).classed('hover', false);
+      tree.legend.filter((leg) => d.id === leg).classed('hover', false);
+    });
+
+    tree.legend.on('mouseenter', (d) => {
+      // Add logic that should happen on start of hovering over a legend here
+      tree.hulls.filter((hull) => d === hull.id).classed('hover', true);
+      tree.legend.filter((leg) => d === leg).classed('hover', false);
+    });
+
+    tree.legend.on('mouseleave', (d) => {
+      // Add logic that should happen on end of hovering over a legend here
+      tree.hulls.filter((hull) => d === hull.id).classed('hover', false);
+      tree.legend.filter((leg) => d === leg).classed('hover', false);
+    });
+  }
+}
+
+registerEvents();
+
+setup_helper();
